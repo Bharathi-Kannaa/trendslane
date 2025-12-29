411e711ae372923dc31f28cdf4718acc8ff7e1a3
BharathiKannaaSP <spbharathikannaa4501@gmail.com>
2025-12-29 18:07:25 +0530

    Turbo repo init

---
+++ apps/web/next.config.ts
@@ -0,0 +1,28 @@
+import type { NextConfig } from 'next';
+import path from 'path';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  transpilePackages: ['@workspace/ui'],
+
+  turbopack: {
+    root: path.join(__dirname, '../..'),
+  },
+
+  images: {
+    dangerouslyAllowSVG: true,
+    remotePatterns: [
+      {
+        hostname: 'shop.mango.com',
+      },
+      {
+        hostname: 'images.pexels.com',
+      },
+      {
+        hostname: 'res.cloudinary.com',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
